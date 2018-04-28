0bfb0e323024fc9017074a58df1c9bb82f1e84d9
johnson <johnson.lee3699@gmail.com>
2018-04-28 20:36:57 +0800

    please

--- WebContent/asset/txn/register/register.js
+++ WebContent/asset/txn/register/register.js
@@ -1,4 +1,4 @@
-eApp.controller('registerController', function ($scope, $http, $controller, sysInfoService, $q, $http) {
+eApp.controller('registerController', function ($scope, $http, $controller, sysInfoService, $q) {
     
     $controller('baseController', {
         $scope: $scope
@@ -8,14 +8,58 @@ eApp.controller('registerController', function ($scope, $http, $controller, sysI
     var log = console.log;
     var table = console.table;
     
+    $scope.register = function() {
+        debugger
+    	$http({
+    		method: 'POST',
+    		url: 'member/register',
+    		data: $scope.registerForm
+    	}).then(function successCallback(response) {
+            debugger
+    		table(response.data);
+    		angular.forEach(response.data, function(obj,index) {
+    			alert(obj.id + ' : ' + obj.name);
+    		})
+    		
+    	}, function errorCallback(response) {
+    		debugger
+    	});
+    };
+    
+    $scope.login = function() {
+        debugger
+    	$http({
+    		method: 'POST',
+    		url: 'member/login',
+    		data: $scope.loginForm
+    	}).then(function successCallback(response) {
+            debugger
+    		table(response.data);
+    		angular.forEach(response.data, function(obj,index) {
+    			alert(obj.id + ' : ' + obj.name);
+    		})
+    		
+    	}, function errorCallback(response) {
+    		debugger
+    	});
+    };
+    
     $scope.init = function () {
-//        table(sysInfoService);
-//        sysInfoService.setUserName('Johnson'); 帥的
-        
-        $scope.main = {
-            data: [],
-            param: []
-        };
-    }
+    	$scope.registerForm = {
+    		account: '',
+            password: '',
+            name: '',
+            sex: '',
+            birthday: '',
+            phone: '',
+            addr: ''
+    	};
+    	$scope.loginForm = {
+            account: '',
+            password: ''
+    	};
+    };
+    
+    
     $scope.init();
 });
