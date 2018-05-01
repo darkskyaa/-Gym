ea0447265e0e9c9a6a79ec2f6707b409661f738c
johnson <johnson.lee3699@gmail.com>
2018-05-01 14:33:10 +0800

    add msgbar wait for adjust

---
+++ WebContent/assets/js/controllers/initController.js
@@ -0,0 +1,15 @@
+eApp.controller('initController', function ($scope, $http, $controller, sysInfoService, $q, $http) {
+    
+    $controller('baseController', {
+        $scope: $scope
+    });
+    $scope.controllerName = 'initController';
+    
+    var log = console.log;
+    var table = console.table;
+    
+    $scope.init = function () {
+    	$scope.container = 'assets/txn/register/register.html';
+    }
+    $scope.init();
+});
