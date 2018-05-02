ac3a83c1aeecc7e02da3f758c5637bdbc9889a3e
johnson <johnson.lee3699@gmail.com>
2018-05-02 18:24:43 +0800

    update something

--- WebContent/assets/js/directives/msgBar.js
+++ WebContent/assets/js/directives/msgBar.js
@@ -19,6 +19,7 @@ eApp.directive('msgBar', function() {
 					            			'<div class="row">'+
 					            				'<div class="col-lg-1 col-md-1 col-sm-1"></div>'+
 					            				'<div id="mwt_msg_panl" class="col-lg-11 col-md-11 col-sm-11">'+
+					            					'<div id="e-msgbar-clear" class="text-right" ng-if="msglist.length>0"><span ng-click="remove(null,true)"><i class="glyphicon glyphicon-trash"></i> 全部清除</span></div>'+
 					            					'<div ng-init="insert(row)" id="mwt_msg_box" style="border-radius:5px!important;line-height:25px;text-align:left;animation: fadeIn .5s;" ng-repeat="row in msglist" ng-class="{\'alert alert-success\': row.type == \'S\',\'alert alert-info\': row.type == \'N\', \'alert alert-warning\': row.type==\'W\', \'alert alert-danger\': row.type==\'E\'}"><a class="close" style="color:#000;" ng-click="remove(row)">&times;</a><span ng-class="{\'label label-info\': row.type == \'N\', \'label label-success\': row.type == \'S\', \'label label-warning\': row.type == \'W\', \'label label-danger\': row.type == \'E\'}">{{row.label}}</span>&nbsp;{{row.message}}</div>'+
 					            				'</div>'+
 				            				'</div>'+
@@ -34,10 +35,14 @@ eApp.directive('msgBar', function() {
         	/* Initialize */   	
         	$scope.msglist = [];
         	/* Remove MsgBox */
-        	$scope.remove = function(row){
-    			for(var i=0; i<$scope.msglist.length; i+=1){
-    				if($scope.msglist[i] == row)$scope.msglist.splice(i, 1);
-    			};
+        	$scope.remove = function(row, removeAll){
+        		if(removeAll) {
+        			$scope.msglist = [];
+        		}else {
+        			for(var i=0; i<$scope.msglist.length; i+=1){
+        				if($scope.msglist[i] == row)$scope.msglist.splice(i, 1);
+        			}
+        		}
     			//Close MsgBox
     			if($scope.msglist.length>0)return;
     			var w = $("#mwt_slider_content").width();
@@ -81,6 +86,7 @@ eApp.directive('msgBar', function() {
     	    		};
 //    	    		alert(' tmpTP: '+tmpTP+' tmpTXT: '+tmpTXT);
     	    		$scope.msglist.unshift({'type': tmpTP, 'message': msgBody, 'label': tmpTXT});
+    	    		if($scope.msglist.length > 3) $scope.msglist.length = 3;
     	    	}
     		};
     		/** append row in list **/
@@ -113,9 +119,8 @@ eApp.directive('msgBar', function() {
     			//level 1
     			$("#mwt_fb_tab>a>span>i, #mwt_fb_tab>a>span").animate({color:'hsla(350,99%,55%,0.9)'},300).animate({color:'#FFF'},300);
 				//level 2
-    			$(e).animate({color:_color_chg},300).animate({color:_color},300).animate({color:_color_chg},300).animate({color:_color},300);
-    			if ($("#mwt_mwt_slider_scroll").css('left') == '-'+w+'px')
-				{
+    			$(e).not("#e-msgbar-clear").animate({color:_color_chg},300).animate({color:_color},300).animate({color:_color_chg},300).animate({color:_color},300);
+    			if ($("#mwt_mwt_slider_scroll").css('left') == '-'+w+'px') {
 					$("#mwt_mwt_slider_scroll").animate({ left:'0px' }, 500 ,'swing');
 					$("#mwt_fb_tab i").removeClass("glyphicon-chevron-right");
 					$("#mwt_fb_tab i").addClass("glyphicon-chevron-left");
@@ -127,8 +132,7 @@ eApp.directive('msgBar', function() {
         }],
         link: function (scope, element, attrs, ctlModel, transclude) {
         	
-        	var w = $("#mwt_slider_content").width(),
-                isLocal = document.location.hostname == "127.0.0.1" || document.location.hostname == "192.168.0.8"  || document.location.hostname == "localhost";
+        	var w = $("#mwt_slider_content").width();
         	
         	$("#mwt_fb_tab").attr("title","滑鼠點擊展開或隱藏訊息視窗").on("mouseenter",function(){
         		$("#mwt_fb_tab").addClass("mwt_fb_tab-hover","fast");
@@ -158,17 +162,14 @@ eApp.directive('msgBar', function() {
         	
         	/** Open/Close by Click **/
         	$("#mwt_fb_tab").click(function(){
-        		if ($("#mwt_mwt_slider_scroll").css('left') == '-'+w+'px')
-        		{
+        		if ($("#mwt_mwt_slider_scroll").css('left') == '-'+w+'px') {
         			//Open
         			$("#mwt_mwt_slider_scroll").animate({ left:'0px' }, 500 ,'swing');
         			$("#mwt_fb_tab i").removeClass("glyphicon-chevron-right");
         			$("#mwt_fb_tab i").addClass("glyphicon-chevron-left");
         			$("#mwt_fb_tab a").removeClass("left_no");
         			$("#mwt_fb_tab a").addClass("left_go");
-        		}
-        		else
-        		{
+        		} else {
         			//Close
         			$("#mwt_mwt_slider_scroll").animate( { left:'-'+w+'px' }, 500 ,'swing');
         			$("#mwt_fb_tab i").removeClass("glyphicon-chevron-left");
@@ -179,14 +180,12 @@ eApp.directive('msgBar', function() {
         	});       	
         	
         	/** Get MSG **/
-        	/* Add: 2016/03/22 ArthurKO */
 //        	scope.$watch('msglist', function (newValue, oldValue) {
 //        		if(newValue == oldValue){
 //        			return;
 //        		};
 //        	};
         	
-        	/* Add: 2016/02/22 ArthurKO */
 //        	$("#mwt_msg_panl").bind('DOMNodeInserted',function(event){
 //        		//Inserted
 //        		if (event.type == 'DOMNodeInserted') {
@@ -211,7 +210,6 @@ eApp.directive('msgBar', function() {
 //        	});
         	
         	/** Close by blur **/
-        	/* Add: 2016/03/02 ArthurKO */
 //        	$("#mwt_slider_content").blur(function(){
 //        		if ($("#mwt_mwt_slider_scroll").css('left') == w+'px'){
 //        			$("#mwt_mwt_slider_scroll").animate( { left:'-'+w+'px' }, 600 ,'swing');
@@ -223,20 +221,15 @@ eApp.directive('msgBar', function() {
 //        	});    
         	
             /** 滑鼠移開移入 **/	
-        	// Modify: 2016/03/02 ArthurKO
-        	// Add: 2016/07/26 ArthurKO isLocal
-        	if(isLocal){
-        		$("#mwt_slider_content").mouseleave(function() {
-            		$("#mwt_mwt_slider_scroll").animate( { left:'-'+w+'px' }, 500 ,'swing');
-            		$("#mwt_fb_tab i").removeClass("glyphicon-chevron-left");
-            		$("#mwt_fb_tab i").addClass("glyphicon-chevron-right");
-            		$("#mwt_fb_tab a").removeClass("left_go");
-            		$("#mwt_fb_tab a").addClass("left_no");
-            	});
-        	}
+        	$("#mwt_slider_content").mouseleave(function() {
+            	$("#mwt_mwt_slider_scroll").animate( { left:'-'+w+'px' }, 500 ,'swing');
+            	$("#mwt_fb_tab i").removeClass("glyphicon-chevron-left");
+            	$("#mwt_fb_tab i").addClass("glyphicon-chevron-right");
+            	$("#mwt_fb_tab a").removeClass("left_go");
+            	$("#mwt_fb_tab a").addClass("left_no");
+            });
         	
         	/** 無訊息自動關閉 **/
-        	// Add: 2016/07/27 ArthurKO
 //        	scope.$watch('msglist', function (newValue, oldValue) {
 //        		alert("in");
 //        		if(scope.msglist.length>0)return;
@@ -272,14 +265,14 @@ eApp.directive('msgBar', function() {
         	$("#mwt_fb_tab").css("z-index","103") //51->102
         					.css("position","absolute")
         					.css("left","0")
-        					.css("margin-bottom","25px")
+        					.css("margin-bottom","2.5rem")
 							.css("background","rgb(254,187,187)")
 							.css("background","-moz-linear-gradient(-45deg,  rgba(254,187,187,1) 0%, rgba(255,92,92,1) 100%)")
 							.css("background","-webkit-linear-gradient(-45deg,  rgba(254,187,187,1) 0%,rgba(255,92,92,1) 100%)")
 							.css("background","linear-gradient(135deg,  rgba(254,187,187,1) 0%,rgba(255,92,92,1) 100%)")
 							.css("filter","progid:DXImageTransform.Microsoft.gradient( startColorstr='#febbbb', endColorstr='#ff5c5c',GradientType=1 )")
 //        					.css("background","hsla(0, 100%, 50%, 0.5)")
-        					.css("border","0px")
+        					.css("border","0")
 //        					.css("text-shadow","0 0 10px rgba(255,255,255,1)")
 							.css("-webkit-box-shadow","rgb(206,220,231) 0px 2px 6px")
 	   						.css("-moz-box-shadow","rgb(206,220,231) 0px 2px 6px")
@@ -299,8 +292,8 @@ eApp.directive('msgBar', function() {
 			   						.css("-webkit-box-shadow","rgb(206,220,231) 0px 2px 6px")
 			   						.css("-moz-box-shadow","rgb(206,220,231) 0px 2px 6px")
 			   						.css("box-shadow","rgb(206,220,231) 0px 2px 6px")
-			   						.css("background","hsla(0, 90%, 40%, 0.1)");
-//        							.css("background","hsla(0,0%,100%,.1)");
+			   						//.css("background","hsla(0, 90%, 40%, 0.1)");
+        							.css("background","hsla(0,0%,100%,.1)");
 			   					
 
         	
